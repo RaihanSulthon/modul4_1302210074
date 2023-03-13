5157d13a9cb6fd9fd8b65104c342e298ed655f95
PC32-IFLAB <PC32-IFLAB>
2023-03-13 08:02:23 +0700

    menambahkan PosisiKarakterGame

--- modul4_1302210074/Program.cs
+++ modul4_1302210074/Program.cs
@@ -13,6 +13,10 @@ namespace modul4_1302210074
             Console.WriteLine("Nama Semua Buah dan Namanya");
             dict_kode_buah.getallbuah();
             Console.WriteLine("\n");
+
+            Console.WriteLine("Status Posisi");
+            PosisiKarakterGame posisi = new PosisiKarakterGame();
+            posisi.key();
             
         }
     }
@@ -53,6 +57,62 @@ namespace modul4_1302210074
             {
                 Console.WriteLine(i.Key + " : " + i.Value);
             }
+        }
+    }
+
+    class PosisiKarakterGame
+    {
+        enum state { Tengkurap, Jongkok, Berdiri, Terbang};
+
+        public void key()
+        {
+            state state= state.Terbang;
+
+            string[] status_posisi = { "Tengkurap", "Jongkok", "Berdiri", "Terbang" };
+            do
+            {
+                Console.WriteLine("Posisi" + " " + status_posisi[(int)state]);
+                Console.WriteLine("Command : ");
+                String command = Console.ReadLine();
+                switch (state)
+                {
+                    case state.Tengkurap:
+                        if(command == "w")
+                        {
+                            state = state.Jongkok;
+                        }
+                        break;
+                case state.Jongkok:
+                        if(command == "w")
+                        {
+                            state = state.Berdiri;
+                        }else if(command == "S")
+                        {
+                            state = state.Tengkurap;
+                        }
+                        break;
+                case state.Berdiri:
+                        if(command == "w")
+                        {
+                            state = state.Terbang;
+                            Console.WriteLine("Posisi take off");
+                        }else if(command == "S")
+                        {
+                            state = state.Jongkok;
+                        }
+                        break;
+                case state.Terbang:
+                        if(command == "x")
+                        {
+                            state= state.Jongkok;
+                            Console.WriteLine("Posisi Landing");
+                        }else if(command == "S")
+                        {
+                            state = state.Berdiri;
+                        }
+                        break;
+                }
+            }while(true);
         }
     }
 }
